0c6649584cf7f0e892a9a5eda8d2a16e2c7926e4
fabiancoronel <fabiancoronel01@gmail.com>
2022-09-13 18:14:21 -0500

    Ajuste conexión. base de datos

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,4 @@
-import sql from 'mssql';
+const sql = require('mssql')
 
 // Configuración de la base de datos
 const dbSettings = {
@@ -11,10 +11,27 @@ const dbSettings = {
 		trustServerCertificate: true,
 	},
 };
+const sqlConfig = {
+  user: 'jovenesmvm',
+  password: '0^618Bjt37y@',
+  database: 'gestionsolicitudes',
+  server: 'gestionsolicitudes.database.windows.net',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: false // change to true for local dev / self-signed certs
+  }
+}
 
 async function getConnection() {
 	try {
-		const pool = await sql.connect(dbSettings);
+		const pool = await sql.connect(sqlConfig);
+		const result = await sql.query`select * from Responsable`
+		console.dir(result)
 		return pool;
 	} catch (error) {
 		console.log('Error al conectar con la base de datos', error);
